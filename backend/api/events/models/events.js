4261a1ad858e1a44bdae9d2bb78d2541f4aed6bb
viet34tqc <viet34tqc@gmail.com>
2021-08-25 23:44:54 +0700

    Create events

--- backend/api/events/models/events.js
+++ backend/api/events/models/events.js
@@ -1,8 +1,17 @@
-'use strict';
+const slugify = require("slugify");
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
- * to customize this model
- */
-
-module.exports = {};
+// Slug is automatically created when create or update events.
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      if (data.name) {
+        data.slug = slugify(data.name, { lower: true }); // slug is lowercase.
+      }
+    },
+    beforeUpdate: async (params, data) => {
+      if (data.name) {
+        data.slug = slugify(data.name, { lower: true });
+      }
+    },
+  },
+};
