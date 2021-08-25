01ee0aeacb463e2e0a3d2cca693fdfd981c7f882
viet34tqc <viet34tqc@gmail.com>
2021-08-25 09:19:14 +0700

    Added Strapi backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6c50f52456923f6a389fed7a37888010'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '97d742db-d25d-41b5-854e-03341f9ee58e'
+};
