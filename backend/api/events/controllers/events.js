9f01d34815c1e27def665aef8f39525e2bfb8934
viet34tqc <viet34tqc@gmail.com>
2021-08-30 10:17:57 +0700

    Create events'custom endpoints that get user events

--- backend/api/events/controllers/events.js
+++ backend/api/events/controllers/events.js
@@ -1,8 +1,29 @@
-'use strict';
+const { sanitizeEntity } = require("strapi-utils");
+
+("use strict");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  // Get events of logged in user
+  async me(ctx) {
+    // Get the user first
+    const user = ctx.state.user;
+    if (!user) {
+      return ctx.badRequest(null, [
+        { message: [{ id: "No authorization header was found" }] },
+      ]);
+    }
+
+    // Then find all the events that belongs to this user
+    const events = await strapi.services.events.find({ users_permissions_user: user.id });
+
+    if (!events) {
+      return ctx.notFound();
+    }
+    return sanitizeEntity(events, { model: strapi.models.events });
+  },
+};
